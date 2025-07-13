4ea7818631dace9bf64b56a6f761aa6620d69e50
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-07-13 00:40:31 -0400

    editor setup

---
+++ src/editor.ts
@@ -0,0 +1,71 @@
+import {basicSetup} from "codemirror";
+import {EditorView, Decoration, hoverTooltip} from "@codemirror/view";
+import {RangeSetBuilder, Range, StateEffect, Extension} from "@codemirror/state";
+
+const DEFAULT = `% Store the first letter in the word as a state
+(qInit, a) -> (qA, a, right)
+(qInit, b) -> (qB, b, right)
+
+% Move to the end of the tape
+(qA, a) -> (qA, a, right)
+(qA, b) -> (qA, b, right)
+(qB, a) -> (qB, a, right)
+(qB, b) -> (qB, b, right)
+
+% Backtrack once when we reach the end of the tape
+(qA, ⬚) -> (qEndA, ⬚, left)
+(qB, ⬚) -> (qEndB, ⬚, left)
+
+% Replace the last letter with the first letter and enter the trap state
+(qEndA, a) -> (qHalt, a, right)
+(qEndA, b) -> (qHalt, a, right)
+(qEndB, a) -> (qHalt, b, right)
+(qEndB, b) -> (qHalt, b, right)`;
+
+export const create_editor = () => {
+    return new EditorView({
+        doc: DEFAULT,
+        parent: document.body,
+        extensions: [basicSetup]
+    });
+}
+
+export const create_decoration_range = (start: number, end: number, class_name = "cm-error") => {
+    return Decoration.mark({
+        class: class_name
+    }).range(start, end);
+}
+
+export const apply_decoration_range = (view: EditorView, decoration: Range<Decoration>) => {
+    const builder = new RangeSetBuilder<Decoration>();
+    builder.add(decoration.from, decoration.to, decoration.value);
+    const decorations = builder.finish();
+
+    view.dispatch({
+        effects: StateEffect.appendConfig.of([EditorView.decorations.of(decorations)])
+    });
+}
+
+const MessageHover = (start: number, end: number, message: string) => {
+    return hoverTooltip((view, hoverPos, side) => {
+        if (hoverPos < start || hoverPos >= end) return null;
+
+        return {
+            pos: start,
+            end: end,
+            above: true,
+            create(view) {
+                const dom = document.createElement("div");
+                dom.classList.add("hover-msg");
+                dom.innerText = message;
+                return { dom };
+            }
+        };
+    });
+}
+
+export const add_hover_message = (view: EditorView, message: string, start: number, end: number) => {
+    view.dispatch({
+        effects: StateEffect.appendConfig.of([MessageHover(start, end, message)])
+    });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,63 @@
-import {CharStream, CommonTokenStream} from "antlr4";
+import "./style.css";
+
+import {CharStream, CommonTokenStream, ErrorListener, Token} from "antlr4";
 import TuringLexer from "./grammar/TuringLexer";
 import TuringParser from "./grammar/TuringParser";
 import TuringExecutor from "./TuringExecutor";
 
-const input = "% Store the first letter in the word as a state\n" +
-    "(qInit, a) -> (qA, a, right)\n" +
-    "(qInit, b) -> (qB, b, right)\n" +
-    "\n" +
-    "% Move to the end of the tape\n" +
-    "(qA, a) -> (qA, a, right)\n" +
-    "(qA, b) -> (qA, b, right)\n" +
-    "(qB, a) -> (qB, a, right)\n" +
-    "(qB, b) -> (qB, b, right)\n" +
-    "\n" +
-    "% Backtrack once when we reach the end of the tape\n" +
-    "(qA, ⬚) -> (qEndA, ⬚, left)\n" +
-    "(qB, ⬚) -> (qEndB, ⬚, left)\n" +
-    "\n" +
-    "% Replace the last letter with the first letter and enter the trap state\n" +
-    "(qEndA, a) -> (qHalt, a, right)\n" +
-    "(qEndA, b) -> (qHalt, a, right)\n" +
-    "(qEndB, a) -> (qHalt, b, right)\n" +
-    "(qEndB, b) -> (qHalt, b, right)";
-const chars = new CharStream(input);
-const lexer = new TuringLexer(chars);
-const tokens = new CommonTokenStream(lexer);
-const parser = new TuringParser(tokens);
-const tree = parser.program();
-console.log(tree.toStringTree(parser.ruleNames, parser));
-tree.accept(new TuringExecutor())
+import {create_editor, create_decoration_range, apply_decoration_range, add_hover_message} from "./editor";
+import {EditorView} from "@codemirror/view";
+
+let editor: EditorView;
+
+document.addEventListener("DOMContentLoaded", () => {
+    editor = create_editor();
+});
+
+class ThrowingErrorListener implements ErrorListener<any> {
+    syntaxError(
+        recognizer: unknown,
+        offendingSymbol: Token | null,
+        line: number,
+        charPositionInLine: number,
+        msg: string,
+        e: Error | undefined
+    ): void {
+        let message = `Syntax error at line ${line}, column ${charPositionInLine}: ${msg}`;
+        if (offendingSymbol) {
+            const decoration = create_decoration_range(offendingSymbol.start, offendingSymbol.start + offendingSymbol.text.length, "cm-error");
+            apply_decoration_range(editor, decoration);
+
+            add_hover_message(editor, message, offendingSymbol.start, offendingSymbol.start + offendingSymbol.text.length);
+
+            // TODO: clear decorations on change
+        }
+
+        throw new Error(message);
+    }
+}
+
+const run = (input: string) => {
+    const chars = new CharStream(input);
+    const lexer = new TuringLexer(chars);
+    const tokens = new CommonTokenStream(lexer);
+
+    const parser = new TuringParser(tokens);
+    parser.removeErrorListeners();
+    parser.addErrorListener(new ThrowingErrorListener());
+
+    const tree = parser.program();
+    console.log(tree.toStringTree(parser.ruleNames, parser));
+
+    tree.accept(new TuringExecutor());
+}
+
+// bind run
+document.getElementById("run")!.addEventListener("click", () => {
+    const input = editor.state.doc.toString();
+    try {
+        run(input);
+    } catch (error: any) {
+        console.error(error);
+    }
+});
