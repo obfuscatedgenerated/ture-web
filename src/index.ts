325b2cd15b7678c1472b0303fa42cbeb478cbf64
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-07-17 22:26:27 +0100

    working keys

--- src/index.ts
+++ src/index.ts
@@ -584,5 +584,109 @@ document.querySelectorAll(".mac-cmd").forEach((el) => {
     }
 });
 
+const override_kbd_hiding = () => {
+    // if the hide media query applies, inject an override !important rule to keep the kbd elements visible
+    const media = window.matchMedia("(hover: none) and (pointer: coarse)");
+    if (!media.matches) {
+        return; // no override needed
+    }
+
+    const style = document.createElement("style");
+    style.innerHTML = `
+        kbd, .kbd-plus {
+            display: inline-block !important;
+        }
+    `;
+    document.head.appendChild(style);
+}
+
+// listen for keybinds
+document.addEventListener("keydown", (e) => {
+    const is_mac = navigator.platform.startsWith("Mac");
+    const control_key = is_mac ? e.metaKey : e.ctrlKey;
+
+    if (control_key && !e.altKey) {
+        // download: Ctrl + S
+        if (e.key === "s" || e.key === "S") {
+            e.preventDefault();
+            override_kbd_hiding();
+
+            download_file();
+            return;
+        }
+
+        // upload: Ctrl + O
+        if (e.key === "o" || e.key === "O") {
+            e.preventDefault();
+            override_kbd_hiding();
+
+            open_file_uploader();
+            return;
+        }
+    }
+
+    if (!control_key && e.altKey) {
+        // copy empty: Alt + E
+        if (e.key === "e" || e.key === "E") {
+            e.preventDefault();
+            override_kbd_hiding();
+
+            navigator.clipboard.writeText(EMPTY).then(() => {
+                copy_empty.innerText = "Copied!";
+                setTimeout(() => {
+                    copy_empty.innerHTML = copy_empty_content;
+                }, 2000);
+            });
+            return;
+        }
+    }
+
+    if (control_key && e.altKey) {
+        // share: Ctrl + Alt + S
+        if (e.key === "s" || e.key === "S") {
+            e.preventDefault();
+            override_kbd_hiding();
+
+            const share_url = get_share_url();
+            navigator.clipboard.writeText(share_url).then(() => {
+                share_button.innerText = "Copied!";
+                setTimeout(() => {
+                    share_button.innerHTML = share_button_content;
+                }, 2000);
+            }).catch((err) => {
+                console.error("Failed to copy share URL: ", err);
+            });
+            return;
+        }
+    }
+
+    // run: F8
+    if (e.key === "F8") {
+        e.preventDefault();
+        override_kbd_hiding();
+
+        const input = editor.state.doc.toString();
+        run(input);
+        return;
+    }
+
+    // run step: F9
+    if (e.key === "F9") {
+        e.preventDefault();
+        override_kbd_hiding();
+
+        run_step();
+        return;
+    }
+
+    // cancel: Esc
+    if (e.key === "Escape") {
+        e.preventDefault();
+        override_kbd_hiding();
+
+        // TODO
+    }
+});
+
 // TODO: split this file up
 // TODO: improve validation error ux when editing file (would help if they had a line to blame)
