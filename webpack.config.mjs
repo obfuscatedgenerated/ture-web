74ad76070c099cfe9ff599c33cfe974196d0d14e
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-07-19 18:49:49 +0100

    improve webpack scripts

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -17,12 +17,6 @@ const __dirname = path.dirname(__filename);
 const is_server = process.env.WEBPACK_SERVE === "true";
 const css_strategy = !is_server ? MiniCssExtractPlugin.loader : "style-loader";
 
-// note: service worker generation not enabled if hmr enabled
-// unregister the sw and restart the server if changing this setting
-// with hmr off, unregister the sw and reload when you make a change
-const USE_HMR = true;
-const using_hmr = is_server && USE_HMR;
-
 console.log("css_strategy:", css_strategy);
 
 const TITLE = "Ture";
@@ -38,118 +32,126 @@ const get_commit_details = () => {
     }
 }
 
-export default {
-    entry: "./src/index.ts",
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/i,
-                use: [css_strategy, "css-loader"],
-            },
-            {
-                test: /\.md$/,
-                use: [
-                    {
-                        loader: "html-loader",
-                    },
-                    {
-                        loader: "markdown-loader",
-                    },
-                ],
-            },
-            {
-                test: [/\.png$/, /\.jpg$/, /\.jpeg$/, /\.gif$/, /\.svg$/],
-                type: "asset/resource",
-                generator: {
-                    filename: "manifest/[name]-[contenthash:8][ext][query]",
+export default (env, argv) => {
+    // note: service worker generation not enabled if hmr enabled
+    // unregister the sw and restart the server if changing this setting
+    // with hmr off, unregister the sw and reload when you make a change
+    const USE_HMR = argv.hot;
+    const using_hmr = is_server && USE_HMR;
+
+    return {
+        entry: "./src/index.ts",
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: "ts-loader",
+                    exclude: /node_modules/,
                 },
-            }
-        ],
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"],
-    },
-    output: {
-        filename: "[name].[contenthash].bundle.js",
-        path: path.resolve(__dirname, "dist"),
-        publicPath: "/",
-    },
-    devServer: {
-        hot: USE_HMR,
-        port: 3000,
-        static: {
-            directory: path.join(__dirname, "public"),
-            publicPath: "/public",
+                {
+                    test: /\.css$/i,
+                    use: [css_strategy, "css-loader"],
+                },
+                {
+                    test: /\.md$/,
+                    use: [
+                        {
+                            loader: "html-loader",
+                        },
+                        {
+                            loader: "markdown-loader",
+                        },
+                    ],
+                },
+                {
+                    test: [/\.png$/, /\.jpg$/, /\.jpeg$/, /\.gif$/, /\.svg$/],
+                    type: "asset/resource",
+                    generator: {
+                        filename: "manifest/[name]-[contenthash:8][ext][query]",
+                    },
+                }
+            ],
         },
-    },
-    plugins: [
-        // note: doesnt update on watch mode but doesnt matter
-        new webpack.DefinePlugin({
-            __COMMIT_DETAILS__: JSON.stringify(get_commit_details()),
-            __USE_SW__: !using_hmr,
-        }),
-
-        new HtmlWebpackPlugin({
-            title: LONG_TITLE,
-            template: "./src/index.html",
-            templateParameters: {
-                commit_details: get_commit_details(),
+        resolve: {
+            extensions: [".tsx", ".ts", ".js"],
+        },
+        output: {
+            filename: "[name].[contenthash].bundle.js",
+            path: path.resolve(__dirname, "dist"),
+            publicPath: "/",
+        },
+        devServer: {
+            hot: USE_HMR,
+            port: 3000,
+            static: {
+                directory: path.join(__dirname, "public"),
+                publicPath: "/public",
             },
-        }),
+        },
+        plugins: [
+            // note: doesnt update on watch mode but doesnt matter
+            new webpack.DefinePlugin({
+                __COMMIT_DETAILS__: JSON.stringify(get_commit_details()),
+                __USE_SW__: !using_hmr,
+            }),
 
-        new MiniCssExtractPlugin({
-            filename: "[name].[contenthash].css",
-        }),
+            new HtmlWebpackPlugin({
+                title: LONG_TITLE,
+                template: "./src/index.html",
+                templateParameters: {
+                    commit_details: get_commit_details(),
+                },
+            }),
 
-        new AppManifestPlugin({
-            content: {
-                name: TITLE,
-                short_name: TITLE,
-                background_color: "#111",
-                start_url: "/",
-                display: "standalone",
-            },
-            destination: "/manifest",
-            useDigest: false
-        }),
+            new MiniCssExtractPlugin({
+                filename: "[name].[contenthash].css",
+            }),
 
-        // disable service worker if hmr is enabled
-        ...(!using_hmr ? [
-            new WorkboxPlugin.GenerateSW({
-                clientsClaim: true,
-                skipWaiting: true,
-                runtimeCaching: [
-                    {
-                        // use network first for index.html
-                        urlPattern: ({request}) => request.mode === "navigate",
-                        handler: "NetworkFirst",
-                        options: {
-                            cacheName: "html-cache",
+            new AppManifestPlugin({
+                content: {
+                    name: TITLE,
+                    short_name: TITLE,
+                    background_color: "#111",
+                    start_url: "/",
+                    display: "standalone",
+                },
+                destination: "/manifest",
+                useDigest: false
+            }),
+
+            // disable service worker if hmr is enabled
+            ...(!using_hmr ? [
+                new WorkboxPlugin.GenerateSW({
+                    clientsClaim: true,
+                    skipWaiting: true,
+                    runtimeCaching: [
+                        {
+                            // use network first for index.html
+                            urlPattern: ({request}) => request.mode === "navigate",
+                            handler: "NetworkFirst",
+                            options: {
+                                cacheName: "html-cache",
+                            },
                         },
-                    },
-                    {
-                        // use standard caching for all other assets (as they are hashed)
-                        urlPattern: /.*/,
-                        handler: "StaleWhileRevalidate",
-                        options: {
-                            cacheName: "asset-cache",
+                        {
+                            // use standard caching for all other assets (as they are hashed)
+                            urlPattern: /.*/,
+                            handler: "StaleWhileRevalidate",
+                            options: {
+                                cacheName: "asset-cache",
+                            },
                         },
-                    },
-                ],
+                    ],
 
-                // forcibly cache the public/precache directory
-                additionalManifestEntries: [
-                    ...glob.sync("public/precache/**/*", {cwd: __dirname, nodir: true}).map(file => ({
-                        url: `/${file}`,
-                        revision: null,
-                    })),
-                ],
-            })
-        ] : []),
-    ]
+                    // forcibly cache the public/precache directory
+                    additionalManifestEntries: [
+                        ...glob.sync("public/precache/**/*", {cwd: __dirname, nodir: true}).map(file => ({
+                            url: `/${file}`,
+                            revision: null,
+                        })),
+                    ],
+                })
+            ] : []),
+        ]
+    };
 }
